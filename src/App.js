1394ccac56b41f1db21900a7c73d0032edd2605d
Jonathan Cain <jcainuk@gmail.com>
2022-09-27 15:39:15 +0100

    Change default formatting added more to my ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-     <ExpenseItem></ExpenseItem>
-    </div>
-  );
-}
+  return <ExpenseItem />;
+};
 
 export default App;
