ab8854b00b1647167d679abecb01bef20d6e3d08
Jonathan Cain <jcainuk@gmail.com>
2022-09-27 16:24:05 +0100

    add title and duplicate ExpenseItems to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
-  return <ExpenseItem />;
+  return (
+    <div>
+      <h2>Let's get started</h2>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
 };
 
 export default App;
